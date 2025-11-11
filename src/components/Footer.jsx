0924207d6f8a59ff40a2ab6c4311948b6af70d00
Julian M. Gonzalez <groverush@gmail.com>
2025-11-10 22:04:05 -0500

    Adding linkedin link to the footer description c:

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import "../styles/footer.css";
 export const Footer = () => {
 	return (
 		<footer className="footer">
-			<p>coded by Julian Monroy</p>
+			<p>coded by <a href="https://www.linkedin.com/in/brayan-julian-monroy-gonzalez-desarrollador-de-videojuegos/">Julian Monroy</a></p>
 		</footer>
 	);
 };
